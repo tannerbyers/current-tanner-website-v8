f1276d0e51edc1f2b72e280dd32f156d09c8f89c
Tanner Byers <38148103+tannerbyers@users.noreply.github.com>
2025-12-19 18:26:31 +0000

    Fix Eleventy output path to write to repository _site (Vercel)

--- .eleventy.js
+++ .eleventy.js
@@ -188,7 +188,7 @@ module.exports = function (eleventyConfig) {
       input: "src",
       includes: "../_includes",
       data: "../_data",
-      output: "../_site",
+      output: "./_site",
     },
   };
 };
