a7120cc8e86fe084c9ccd613dfe52116be90b6ac
Tanner Byers <programtanner@gmail.com>
2026-03-30 15:19:07 -0400

    Downgrade Tina to 2.1.0 to fix Vite build error

--- tina/__generated__/types.ts
+++ tina/__generated__/types.ts
@@ -621,7 +621,7 @@ export const ExperimentalGetTinaClient = () =>
   getSdk(
     generateRequester(
       createClient({
-        url: "https://content.tinajs.io/2.2/content/009017c7-2542-41bd-b8b9-ac0096497c08/github/main",
+        url: "https://content.tinajs.io/2.0/content/009017c7-2542-41bd-b8b9-ac0096497c08/github/main",
         queries,
       })
     )
